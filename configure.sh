215f49f76b221d11951bee4d007b72f3427e8ece
alinajimahalleh <anm33918@gmail.com>
2022-09-29 17:39:51 +0330

    Configure.sh file updated and now u can use ninja

--- configure.sh
+++ configure.sh
@@ -1,2 +1,35 @@
-#! /bin/sh
-cmake -S . -B build/ 
+#! /bin/bash
+
+option=$1
+ninja=1
+case $option in 
+	"release")
+		if [ -d "/build" ]; then
+			mkdir build
+		fi
+		cd build
+		cmake -DCMAKE_BUILD_TYPE=Release -S .. -B .
+		make
+		;;
+	"debug")
+		if [ -d "/build" ]; then
+			mkdir build
+		fi
+		cd build
+		cmake --DCMAKE_BUILD_TYPE=Debug .. -S .. -B .
+		make
+		;;
+	"clean")
+		cd build
+		rm -rf CMakefiles CMakeCache.txt
+		;;
+	"ninja")
+		if [ -d "/build" ]; then
+			mkdir build
+		fi
+		cd build
+		cmake -S .. -G Ninja
+		ninja
+		;;
+esac
+
